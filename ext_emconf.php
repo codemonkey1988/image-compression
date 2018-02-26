2124d4641374eb82b482b36ad5dd44fd9e655559
Tim Schreiner <schreiner@arndtteunissen.de>
2018-02-26 20:26:02 +0100

    Add ext:metadata to dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,6 +7,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints'      => [
         'depends'   => [
             'typo3' => '7.6.0-8.7.99',
+            'metadata' => '7.6.0-8.7.99'
         ],
         'conflicts' => [],
         'suggests'  => []
