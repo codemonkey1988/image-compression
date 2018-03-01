0f54f3530f5f015dd701b16eb72e07478ec20ac1
Tim Schreiner <schreiner@arndtteunissen.de>
2018-03-01 20:40:12 +0100

    Remove ext:filemetadata as dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,8 +6,7 @@ $EM_CONF[$_EXTKEY] = [
     'category'         => 'fe',
     'constraints'      => [
         'depends'   => [
-            'typo3' => '7.6.0-8.7.99',
-            'filemetadata' => '7.6.0-8.7.99'
+            'typo3' => '7.6.0-8.7.99'
         ],
         'conflicts' => [],
         'suggests'  => []
