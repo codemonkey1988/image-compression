5685241eff7de99648a97cac4bbdc0d4141a9246
Tim Schreiner <schreiner@arndtteunissen.de>
2018-03-09 11:21:28 +0100

    Remove deprecated TYPO3 v8 TCA config

--- Configuration/TCA/Overrides/sys_file_metadata.php
+++ Configuration/TCA/Overrides/sys_file_metadata.php
@@ -1,54 +1,26 @@
 <?php
 
-/**
- * Copyright notice
+/*
+ * This file is part of the TYPO3 responsive images project.
  *
- *  (c) 2018 Tim Schreiner <schreiner.tim@gmail.com>
- *  All rights reserved
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  *
- * This script is part of the TYPO3 project. The TYPO3 project is
- * free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * For the full copyright and license information, please read
+ * LICENSE file that was distributed with this source code.
  *
- * The GNU General Public License can be found at
- * http://www.gnu.org/copyleft/gpl.html.
- *
- * This script is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * This copyright notice MUST APPEAR in all copies of the script!
  */
-$boot = function () {
-    $ll = 'LLL:EXT:image_compression/Resources/Private/Language/locallang_db.xlf:';
 
-    if (\TYPO3\CMS\Core\Utility\GeneralUtility::compat_version('8.6.0')) {
-        $config = [
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('sys_file_metadata', [
+    'image_compression_last_compressed' => [
+        'label' => 'LLL:EXT:image_compression/Resources/Private/Language/locallang_db.xlf:sys_file.image_compression_last_compressed',
+        'exclude' => 1,
+        'config' => [
             'type' => 'input',
             'renderType' => 'inputDateTime',
             'dbType' => 'datetime',
             'eval' => 'datetime',
-        ];
-    } else {
-        $config = [
-            'type' => 'input',
-            'size' => '13',
-            'eval' => 'datetime',
-            'default' => '0',
-        ];
-    }
-
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('sys_file_metadata', [
-        'image_compression_last_compressed' => [
-            'label' => $ll . 'sys_file.image_compression_last_compressed',
-            'exclude' => 1,
-            'config' => $config,
         ],
-    ]);
-};
-
-$boot();
-unset($boot);
+    ],
+]);
